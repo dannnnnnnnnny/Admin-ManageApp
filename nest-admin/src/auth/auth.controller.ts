db808bb887262cc7aa12f3409b0cd110317d5b71
ehdgnl5249 <ehdgnl5249@naver.com>
2021-02-19 00:25:28 +0900

    add login API

--- nest-admin/src/auth/auth.controller.ts
+++ nest-admin/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, NotFoundException, Post } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcryptjs';
 import { RegisterDto } from './models/register.dto';
@@ -14,11 +14,30 @@ export class AuthController {
     if (body.password !== body.password_confirm) {
       throw new BadRequestException('비밀번호가 일치하지 않습니다.');
     }
+
     const hashed = await bcrypt.hash(body.password, 12);
+
     return this.userService.create({
       nickname: body.nickname,
       email: body.email,
       password: hashed,
     });
+  }
+
+  @Post('login')
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const user = await this.userService.findOne({ email });
+    if (!user) {
+      throw new NotFoundException('존재하지 않는 유저입니다.');
+    }
+
+    if (!await bcrypt.compare(password, user.password)) {
+      throw new BadRequestException('인증이 유효하지 않습니다.');
+    }
+
+    return user;
   }
 }
